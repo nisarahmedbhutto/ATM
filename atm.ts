8aad0e3a9424dc07e23b589cea2e7924ed21274c
nisarahmedbhutto <bhuttonisar60@gmail.com>
2024-03-28 06:02:40 +0500

    new file:   .gitignore 	new file:   atm.js 	new file:   atm.ts 	new file:   package-lock.json 	new file:   package.json 	new file:   tsconfig.json

---
+++ atm.ts
@@ -0,0 +1,67 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let myBalance: number = 10000;
+let pinCode: number = 22331;
+
+let userAns = await inquirer.prompt([
+  {
+    message: "please enter your pin code",
+    type: "number",
+    name: "userPinCode",
+  },
+]);
+
+if (userAns.userPinCode === pinCode) {
+  console.log("correct your pin code");
+
+  let operation = await inquirer.prompt([
+    {
+      message: "please enter one option",
+      type: "list",
+      name: "option",
+      choices: ["withdraw", "check balance"],
+    },
+  ]);
+
+  if (operation.option === "withdraw") {
+    console.log("please select options");
+
+    let fastCash = await inquirer.prompt([
+      {
+        message: "please select one option",
+        type: "list",
+        name: "select",
+        choices: ["1000", "5000", "10000", "anyAmount"],
+      },
+    ]);
+
+    let amount = 0;
+
+    if (fastCash.select === "anyAmount") {
+      let anyAmount = await inquirer.prompt([
+        {
+          message: "please enter amount",
+          type: "number",
+          name: "takeMoney",
+        },
+      ]);
+
+      amount = anyAmount.takeMoney;
+    } else {
+      amount = fastCash.select;
+    }
+
+    if (amount <= 10000) {
+      myBalance -= amount;
+      console.log(`your remaining balance is ${myBalance}`);
+    } else {
+      console.log("please enter correct amount");
+    }
+  } else if (operation.option === "check balance") {
+    console.log(`your current balance is ${myBalance}`);
+  }
+} else {
+  console.log("incorrect pin code");
+}
